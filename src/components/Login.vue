0c7a088a63bdc486bfc6509d243c095c53870191
goodym <11184629@qq.com>
2020-08-30 12:36:35 +0800

    初始化项目

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,35 +1,35 @@
 <template>
   <div id="login">
     <el-form
-            :model="userLoginForm"
-            :rules="loginRules"
-            status-icon
-            ref="userLoginFormRef"
-            label-position="left"
-            label-width="0px"
-            class="demo-ruleForm login-page"
+      :model="userLoginForm"
+      :rules="loginRules"
+      status-icon
+      ref="userLoginFormRef"
+      label-position="left"
+      label-width="0px"
+      class="demo-ruleForm login-page"
     >
       <h3 class="title">
         墨鱼-后台权限管理系统
       </h3>
       <el-form-item prop="username">
         <el-input
-                type="text"
-                @keyup.enter.native="handleSubmit"
-                v-model="userLoginForm.username"
-                auto-complete="off"
-                placeholder="用户名"
-                prefix-icon="iconfont el-icon-user"
+          type="text"
+          @keyup.enter.native="handleSubmit"
+          v-model="userLoginForm.username"
+          auto-complete="off"
+          placeholder="用户名"
+          prefix-icon="iconfont el-icon-user"
         ></el-input>
       </el-form-item>
       <el-form-item prop="password">
         <el-input
-                @keyup.enter.native="handleSubmit"
-                type="password"
-                v-model="userLoginForm.password"
-                auto-complete="off"
-                placeholder="密码"
-                prefix-icon="el-icon-suitcase-1"
+          @keyup.enter.native="handleSubmit"
+          type="password"
+          v-model="userLoginForm.password"
+          auto-complete="off"
+          placeholder="密码"
+          prefix-icon="el-icon-suitcase-1"
         ></el-input>
       </el-form-item>
       <div></div>
@@ -38,10 +38,10 @@
       <el-form-item style="width:100%;">
         <div style="float:right;">
           <el-button
-                  type="primary"
-                  class="el-icon-mobile-phone"
-                  @click="handleSubmit"
-                  :loading="loading"
+            type="primary"
+            class="el-icon-mobile-phone"
+            @click="handleSubmit"
+            :loading="loading"
           >登录</el-button>
           <el-button class="el-icon-refresh" @click="resetForm">重置</el-button>
         </div>
@@ -50,45 +50,94 @@
 
     <!-- 验证码 -->
     <Vcode
-            :show="isShow"
-            @success="success"
-            @close="close"
-            :canvasWidth="500"
-            :canvasHeight="350"
+      :show="isShow"
+      @success="success"
+      @close="close"
+      :canvasWidth="500"
+      :canvasHeight="350"
     />
 
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="45%">
+      <span>
+        《墨鱼-图书借阅管理系统》 该项目后端采用SpringCloud，Shiro，通用Mapper开发API接口，
+        前端使用当今较为流行的Vue.js。
+      </span>
+      <el-table border :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="账号" width="180"></el-table-column>
+        <el-table-column prop="name" label="密码" width="180"></el-table-column>
+        <el-table-column prop="address" label="描述"></el-table-column>
+      </el-table>
+      <br />一个可供初学者接触的前后端分离项目
+      后端项目：
+      <el-link href="https://github.com/zykzhangyukang/Xinguan" type="primary">厂库链接地址</el-link>&nbsp;&nbsp;&nbsp;
+      前端项目：
+      <el-link href="https://github.com/zykzhangyukang/Vue-Xinguan" type="primary">厂库链接地址</el-link>
+
+      <p>项目不定期更新：因为我比较懒~，觉得不错的话，还望各位大佬给个Star哟~。</p>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">点个赞</el-button>
+        <el-button type="primary" @click="dialogVisible = false">去登入</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import Vcode from "vue-puzzle-vcode";
+
 export default {
+  data() {
+    return {
+      isShow: false,
+      dialogVisible: false,
+      imgCode: undefined,
+      //表单用户登入数据
+      loading: false,
+      userLoginForm: {
+        username: "xueden",
+        password: "123456"
+      },
+      checked: true,
+
+      //验证规则
+      loginRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 12, message: "长度在 3 到 12 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入用户密码", trigger: "blur" },
+          { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" }
+        ]
+      },
+      tableData: [
+        {
+          date: "employee1",
+          name: "123456",
+          address: "系统入库操作员"
+        },
+        {
+          date: "employee2",
+          name: "123456",
+          address: "系统信息维护员"
+        },
+        {
+          date: "employee3",
+          name: "123456",
+          address: "系统监控员"
+        },
+        {
+          date: "employee4",
+          name: "123456",
+          address: "物资审核员"
+        }
+      ]
+    };
+  },
   components: {
     Vcode
   },
-  data() {
-    return{
-      isShow: false,
-     //表单用户登入数据
-     loading: false,
-     userLoginForm: {
-           username: "xueden",
-           password: "123456"
-    },
-    checked: true,
-    //验证规则
-     loginRules: {
-            username: [
-              { required: true, message: "请输入用户名", trigger: "blur" },
-              { min: 3, max: 12, message: "长度在 3 到 12 个字符", trigger: "blur" }
-            ],
-              password: [
-                { required: true, message: "请输入用户密码", trigger: "blur" },
-                { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" }
-              ]
-    }
-    }
-  },
+
   methods: {
     //登入提交
     handleSubmit: function() {
@@ -109,10 +158,10 @@ export default {
       this.loading = true;
       //发起登入请求
       const { data: res } = await this.$http.post(
-              "ucenter/user/login?username=" +
-              this.userLoginForm.username +
-              "&password=" +
-              this.userLoginForm.password
+        "ucenter/user/login?username=" +
+          this.userLoginForm.username +
+          "&password=" +
+          this.userLoginForm.password
       );
       if (res.code == 200) {
         this.$message({
@@ -133,11 +182,9 @@ export default {
       }
       this.loading = false;
     },
-    close() {
-      this.isShow = false;
-    },
+
     /**
-     获取用户信息
+      获取用户信息
      */
     async getUserInfo() {
       const { data: res } = await this.$http.get("ucenter/user/info");
@@ -148,13 +195,13 @@ export default {
       this.$store.commit("setUserInfo", res.data);
       //跳转到home
       this.$router.push("/home");
+    },
+    close() {
+      this.isShow = false;
     }
-
-
-
-
-  }
-}
+  },
+  created() {}
+};
 </script>
 
 <style scoped>
@@ -174,29 +221,31 @@ export default {
     color: #707070;
   }
 
-  .login-container {
-    width: 100%;
-    height: 100%;
+.login-container {
+  width: 100%;
+  height: 100%;
 
-  }
-  #login{
-    position: relative;
-  }
+}
+#login{
+  position: relative;
+}
 
 
-  .login-page {
-    position: relative;
-    -webkit-border-radius: 5px;
-    border-radius: 5px;
-    margin: 190px auto;
-    width: 370px;
-    padding: 40px 35px 15px;
-    background: #fff;
-    border: 1px solid #eaeaea;
-  }
-  label.el-checkbox.rememberme {
-    margin: 0px 0px 15px;
-    text-align: left;
-  }
+.login-page {
+  position: relative;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  margin: 190px auto;
+  width: 370px;
+  padding: 40px 35px 15px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+}
+label.el-checkbox.rememberme {
+  margin: 0px 0px 15px;
+  text-align: left;
+}
 
 </style>
+
+
--- src/main.js
+++ src/main.js
@@ -8,56 +8,74 @@ import NProgress from 'nprogress' // 引入nprogress插件
 import 'nprogress/nprogress.css'  // 这个nprogress样式必须引入
 import axios from 'axios'
 import echarts from 'echarts'
-
+import ZkTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 import { hasPermission } from './utils/permissionDirect'
+
 const Plugins = [ hasPermission ]
 Plugins.map((plugin) => {
-    Vue.use(plugin)
+  Vue.use(plugin)
 })
+
+
+Vue.use(VueQuillEditor)
+Vue.use(ZkTable)
 Vue.use(echarts)
 
 NProgress.configure({ ease: 'ease', speed: 500 });
 NProgress.configure({ minimum: 0.3 });
-
-
-// 给Vue函数添加一个原型属性$http指向Axios
 Vue.prototype.$http = axios
 
+//这是线上的API接口,如果使用本地的可以注释选择本地的服务
+// axios.defaults.baseURL = 'https://www.zykhome.club/api/'
+
 axios.defaults.baseURL = 'http://localhost:9003/'
+
+
 
 /**
  *  axios请求拦截器
  */
 axios.interceptors.request.use(config => {
-      NProgress.start() // 设置加载进度条(开始..)
-      config.headers.Authorization = window.localStorage.getItem('JWT_TOKEN');
-      return config;
-    }
-    , error => {
-      return Promise.reject(error)
-    });
+  NProgress.start() // 设置加载进度条(开始..)
+  config.headers.Authorization = window.localStorage.getItem('JWT_TOKEN');
+  return config;
+}
+  , error => {
+    return Promise.reject(error)
+  });
+
 
 /**
  * axios响应拦截器
  */
 axios.interceptors.response.use(
-    function (response) {
-      NProgress.done() // 设置加载进度条(结束..)
-      if (response.data.code == 4001) {//如果返回的code==4001说明token错误或者token过期
-        window.localStorage.clear();
-        return router.push("/login");
-      }else {
+  function (response) {
+    NProgress.done() // 设置加载进度条(结束..)
+    if (response.data.code == 4001) {//如果返回的code==4001说明token错误或者token过期
+      window.localStorage.clear();
+      // var vm=new Vue();
+      // vm.$message.error("您的登入状态已失效,请您重新登入~")
+      return router.push("/login");
+    }else {
         return response;
-      }
-    },
-    function (error) {
-      return Promise.reject(error)
     }
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
 )
+/**
+ * 自定义权限指令
+ */
+
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
